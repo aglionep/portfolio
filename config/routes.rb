d3f2f39dd187cb850b4358e030c7954513f72719
Paul Aglione <aglionep@gmail.com>
2015-08-04 13:32:50 -0700

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  
+  root 'homepages#index'
+
   # resources :homepages
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  root 'homepages#index'
+  # root 'homepages#index'
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
